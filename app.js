4c2dc7fb1ef264c882b1e1ca47b21c4917544021
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-04-28 16:38:00 +0900

    [Add]5演習問題

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $(function(){
-  $('.box1').hide();
+  $('.box1').slideDown(2000,function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px'
+    }).slideUp(1500);
+  });
 });
